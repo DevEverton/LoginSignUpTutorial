55171a730f9b1c96a8d68aee412e68fc20449d07
Everton Carneiro <everton.iosdev@gmail.com>
2023-09-27 22:45:47 -0300

    Project assets and helper

---
+++ helper/Validations.swift
@@ -0,0 +1,24 @@
+//
+//  Validations.swift
+//  LoginScreen
+//
+//  Created by Everton Carneiro on 23/09/23.
+//
+
+import Foundation
+
+enum Validator {
+    static func validateEmail(_ email: String) -> Bool {
+        let emailRegex = #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
+        
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+
+    static func validatePassword(_ password: String) -> Bool {
+        let passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+        
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+        return passwordPredicate.evaluate(with: password)
+    }
+}
